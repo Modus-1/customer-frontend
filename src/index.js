fae6f125fff16dbdd52029839d02adfd96e82753
Calvin Zhen <418224@student.fontys.nl>
2022-10-06 09:58:14 +0200

    Removed App import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Styling/index.css';
-import App from './Pages/App';
 import LandingPage from './Pages/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
