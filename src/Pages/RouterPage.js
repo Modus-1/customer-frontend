309e4581e2791c80b6683b7d98af52a9e0162f80
Calvin Zhen <418224@student.fontys.nl>
2022-10-07 11:15:40 +0200

    First iteration of menu page

--- src/Pages/RouterPage.js
+++ src/Pages/RouterPage.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import ErrorPage from "./ErrorPage";
+import MenuPage from "./MenuPage";
 
 function RouterPage() {
   return (
@@ -10,6 +11,7 @@ function RouterPage() {
         <Route path="/Home" element=<LandingPage />></Route>
         <Route path="/LandingPage" element=<LandingPage />></Route>
         <Route path="/Error" element=<ErrorPage />></Route>
+        <Route path="/Menu" element={<MenuPage />}></Route>
         <Route path="*" element=<ErrorPage />></Route>
       </Routes>
     </div>
