8fdba6eb948eea16fb27c2d06452f0ccdd1526fc
Calvin Zhen <418224@student.fontys.nl>
2022-10-14 13:52:28 +0200

    Added tests

---
+++ src/__tests__/LandingPage.test.js
@@ -0,0 +1,15 @@
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+import React from "react";
+import "@testing-library/jest-dom";
+import LandingPage from "../Pages/LandingPage";
+import { render, screen } from "@testing-library/react";
+
+test("The landing page displays the correct info", () => {
+  render(<LandingPage />);
+  expect(screen.getByText("Welcome to the Landing page!")).toBeInTheDocument();
+});
