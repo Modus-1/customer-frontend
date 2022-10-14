632369fcb91e5872859b6ea46208ee060413fabf
Calvin Zhen <418224@student.fontys.nl>
2022-10-14 14:36:10 +0200

    Removed old code

--- src/Pages/MenuPage.js
+++ src/Pages/MenuPage.js
@@ -4,42 +4,6 @@ import ResponsiveAppBar from "../Components/CategoryTopBar";
 import MenuItem from "../Components/MenuItem";
 import { getAllMenuItems } from "../Components/Services";
 
-// function RenderMenuItems() {
-//   const [menuItems, setMenuItems] = useState([]);
-
-//   const menuRef = useRef([]);
-
-//   var itemList = [];
-
-//   // const MapMenuItems = useCallback(() => {
-//   //   menuRef.current = menuItems;
-//   //   //console.log(menuRef.current);
-//   //   if (menuRef.current.length > 0) {
-//   //     itemList = menuRef.current.map((item) => {
-//   //       //console.log(item);
-//   //       return <MenuItem key={item.id} dish={item} />;
-//   //     });
-//   //     console.log(itemList);
-//   //     return <div>{itemList}</div>;
-//   //   }
-//   // }, [menuItems]);
-
-//   useEffect(() => {
-//     GetMenuItems();
-//     // menuRef.current = menuItems;
-//   }, []);
-
-//   // useEffect(() => {
-//   //   MapMenuItems();
-//   // }, [menuItems, MapMenuItems]);
-
-//   async function GetMenuItems() {
-//     var menustuff = await getAllMenuItems();
-//     //console.log(menustuff);
-//     setMenuItems(menustuff);
-//   }
-// }
-
 function MenuPage() {
   const [menuItems, setMenuItems] = useState([]);
 
