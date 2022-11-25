87aa639f873354d1efcfa85887d7cab8db535cce
TeunMos <teun.mos@gmail.com>
2022-11-25 14:38:56 +0100

    added comments and console log

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -8,12 +8,19 @@ function LandingPage() {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const tableNumber = urlParams.get('tableNumber');
+    console.log(tableNumber);
+    
+    //1 check if session exists for tableNumber
+    // make request to gateway to check for session related to tableNumber
 
-    //check if session exists for tableNumber
+    //2 if session does not exist, redirect to menu page and create session
+    // get response from gateway whether the session exists
+    // yes? -> 3
+    // no? -> redirect to menu page and create session -> gateway session
 
-    //if session does not exist, redirect to menu page and create session
-
-    //if session exists, do not redirect to menu page and wait for user to login
+    //3 if session exists, do not redirect to menu page and wait for user to login
+    // enter passcode and verify in gateway 
+    // correct passcode? -> join session and redirect to menu page :)
 
   }, []);
 
