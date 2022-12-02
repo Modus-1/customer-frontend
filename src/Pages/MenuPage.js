4d6af7299c1e7ff0c700ee5b84cab5ddf8b03499
Lukas <lukas100@live.nl>
2022-12-02 15:53:16 +0100

    Updated Menupage to new design.

--- src/Pages/MenuPage.js
+++ src/Pages/MenuPage.js
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import "../Styling/MenuPage.css";
 import CategoryTopBar from "../Components/CategoryTopBar";
 import MenuItem from "../Components/MenuItem";
 import { getAllMenuItems } from "../Components/Services";
+import { CheckoutContext } from "../Components/OrderReviewContext";
 
 function MenuPage() {
   const [menuItems, setMenuItems] = useState([]);
   const [activeFilter, setActiveFilter] = useState("");
   const [filteredMenuItems, setFilteredMenuItems] = useState(menuItems);
 
+  const totalOrders = useContext(CheckoutContext).GetTotalOrders();
+
   useEffect(() => {
     GetMenuItems();
   }, []);
+
 
   useEffect(() => {
     let filteredmenu = [];
@@ -26,18 +31,33 @@ function MenuPage() {
     setFilteredMenuItems(filteredmenu);
   }, [menuItems, activeFilter]);
 
+  let navigate = useNavigate();
+
   async function GetMenuItems() {
     let menustuff = await getAllMenuItems();
     setMenuItems(menustuff);
   }
 
   function FilterHandeler(filter) {
+    if (filter === '0'){
+      setActiveFilter("");
+      return;
+    }
     if (activeFilter === filter) {
       setActiveFilter("");
       return;
     }
     setActiveFilter(filter);
   }
+
+  // I know this is stupid but idk how to limit it in CSS, btw blame Calvin for this idea.
+  function getTotalOrders(){
+    if(totalOrders > 99){
+      return '99..'
+    }
+    else return totalOrders
+  }
+
 
   return (
     <div>
@@ -52,10 +72,11 @@ function MenuPage() {
       </div>
       <div className="menupage-footer">
         <div className="basket-image-container">
-          <img src="./assets/basket.svg"></img>
+          <img alt="basket" src="./assets/basket.svg"></img>
+          <div className="basket-amount">{getTotalOrders()}</div>
         </div>
         <div className="menupage-footer-button-container">
-          <button>BETAAL!</button>
+          <button onClick={() => {navigate("/Review")}}>Afrekenen</button>
         </div>
       </div>
     </div>
