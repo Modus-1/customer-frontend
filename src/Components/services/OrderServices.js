b5a273ce30fb5c38eb10f4ca458936e8ffff9353
Hagoort, Bart <42860246+LokiTheShadySpirit@users.noreply.github.com>
2022-12-06 10:40:03 +0100

    added button to send order to api

---
+++ src/Components/services/OrderServices.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export async function MakeOrder(ordernote, totalPrice) {
+  const customConfig = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const bodyFormData = {
+    totalPrice: 0,
+    tableId: 0,
+    note: ordernote,
+  };
+
+  const response = await axios.post(
+    "https://localhost:7116/Order/create/",
+    bodyFormData,
+    customConfig
+  );
+  return response.data.data;
+}
+
+export async function AddItemsToOrder(orderid, orderitems) {
+  let bodyFormData = [];
+  orderitems.map((item) =>
+    bodyFormData.push({
+      id: item.id,
+      amount: item.amount,
+      name: item.name,
+    })
+  );
+
+  const response = await axios.post(
+    `https://localhost:7116/Order/{orderid}/item`,
+    bodyFormData
+  );
+
+  console.log(response);
+}
