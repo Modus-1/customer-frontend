43404395cd8a29c002d6be95221a2e4805e46834
TeunMos <teun.mos@gmail.com>
2022-12-15 15:56:31 +0100

    added popup when adding items to order

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -2,14 +2,30 @@ import React, { useContext, useState } from "react";
 import "../Styling/MenuItem.css";
 import { CheckoutContext } from "../Components/OrderReviewContext";
 import MenuItemInfoPopup from "../Components/MenuItemInfoPopup";
+import Popup from "reactjs-popup";
+import checkMarkImg from "./checkmark.svg";
 
 function MenuItem(dish) {
   const addMenuItem = useContext(CheckoutContext).AddMenuItemToOrder;
   const [isMoreInfoOpen, setIsMoreInfoOpen] = useState(false);
-
-  function AddMenuItemToOrder() {
+  const [isAddMenuItemPopupOpen, setIsAddMenuItemPopupOpen] = useState(false);
+  const popupId = "add-popup";
+  
+  async function AddMenuItemToOrder() {
     addMenuItem(dish.dish);
+    setIsAddMenuItemPopupOpen(true);
+    console.log("here is the popup id: " + popupId); //animation when adding menu item to order
+    await new Promise((r) => setTimeout(r, 10));
+    document.getElementById(popupId).style.translate = "0px 0px";
+    document.getElementById(popupId).style.filter = "opacity(100%)";
+    await new Promise((r) => setTimeout(r, 1000));
+    document.getElementById(popupId).style.filter = "opacity(0%)";
+    await new Promise((r) => setTimeout(r, 500));
+    document.getElementById(popupId).style.translate = "0px -200vh";
+    setIsAddMenuItemPopupOpen(false);
   }
+
+
 
   const { id, name, iconUrl, shortDescription, price } = dish.dish;
   return (
@@ -40,6 +56,18 @@ function MenuItem(dish) {
         </div>
       </div>
       <MenuItemInfoPopup dish={dish.dish} isOpen={isMoreInfoOpen} setOpen={setIsMoreInfoOpen}  addToOrder={AddMenuItemToOrder}/>
+      <Popup open={isAddMenuItemPopupOpen} closeOnDocumentClick={false} modal>
+        <div id={popupId} className="add-menu-item-popup-container">
+          <div className="add-menu-item-popup">
+            <div className="add-menu-item-popup-img">
+              <img src={checkMarkImg} alt="order added image" />
+              </div>
+            <div className="add-menu-item-popup-text">
+              <span>{dish.dish.name} added to order!</span>
+            </div>
+            </div>
+        </div>
+      </Popup>q
     </div>
   );
 }
